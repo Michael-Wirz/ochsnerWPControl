247bee2874f97d67dfeaa64fc1f8cfa5a4d78eaa
wiz <michael.wirz@sympany.ch>
2022-10-14 21:17:41 +0200

    change jpeg Names

--- getBoilerTemp.sh
+++ getBoilerTemp.sh
@@ -10,13 +10,13 @@
  wget http://${esp32_cam_ip}/saved-photo
  #move the save-photo to save-photo.jpeg
  mv saved-photo saved-photo.jpeg
- #rotate and crop the picture and save the output as ready4ocr.jpeg
- convert saved-photo.jpeg -rotate 269 -brightness-contrast -70,0 -crop 1000x500+325+425 -colorspace gray ready4ocr.jpeg
+ #rotate and crop the picture and save the output as afterConvert.jpeg
+ convert saved-photo.jpeg -rotate 269 -brightness-contrast -70,0 -crop 1000x500+325+425 -colorspace gray afterConvert.jpeg
 
  #use textcleaner.sh => makes it easier for the ocr
- ./textcleaner.sh -c 0,0,100,0 ready4ocr.jpeg after-textcleaner.jpeg
+ ./textcleaner.sh -c 0,0,100,0 afterConvert.jpeg afterTextcleaner.jpeg
  #get the boiler temp from the picture with ssocr
- boilerTemp=$(ssocr/ssocr -d 3 after-textcleaner.jpeg)
+ boilerTemp=$(ssocr/ssocr -d 3 afterTextcleaner.jpeg)
  #Problem with nr7, will be recognized as _ so I replace _ with 7
  boilerTemp=${boilerTemp/_/7}
  #decimal dot will not be recognized => divide the result by 10
